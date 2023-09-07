fd15fed6aa5cdbc0690fca4b91af422c12c3a01e
God-Love Mekoudim Medjoussi <gmekoudi@c2r1p2.42sp.org.br>
2023-09-07 16:13:40 -0300

    .

---
+++ ex01/ft_strncpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmekoudi <gmekoudi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 11:13:02 by gmekoudi          #+#    #+#             */
+/*   Updated: 2023/08/29 12:58:51 by gmekoudi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (n > i && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (n > i)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
